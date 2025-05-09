0b4fc1252407cfa22e53d7934362ee8a6cc26c27
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-09 11:49:03 -0300

    refactor: OrderService and OrderResponse to include customerId in order details

--- src/main/java/tech/wvs/desafiobtg/controller/dto/OrderResponse.java
+++ src/main/java/tech/wvs/desafiobtg/controller/dto/OrderResponse.java
@@ -3,5 +3,6 @@ package tech.wvs.desafiobtg.controller.dto;
 import java.math.BigDecimal;
 
 public record OrderResponse(Long orderId,
+                            Long customerId,
                             BigDecimal total) {
 }
